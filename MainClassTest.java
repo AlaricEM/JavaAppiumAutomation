bbb167cfe47c9c9d74dc49189e9e02eba4609bc5
AlaricEM <133274617+AlaricEM@users.noreply.github.com>
2023-10-09 17:02:09 +0300

    Test 2

--- MainClassTest.java
+++ MainClassTest.java
@@ -9,4 +9,14 @@ public class MainClassTest extends MainClass {
             System.out.println("Error!!! getLocalNumber = " + this.getLocalNumber());
         }
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        if (this.getClassNumber() > 45) {
+            System.out.println("Success! Value greater than 45");
+        } else {
+            System.out.println("Error!!! Value = " + this.getClassNumber());
+        }
+    }
 }
